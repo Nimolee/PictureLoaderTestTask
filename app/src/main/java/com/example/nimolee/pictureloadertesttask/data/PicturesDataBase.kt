575cef164838bd7575bd152c28039dc929ecfa5f
Serhiy Kovernik <Serhiy-120397@ukr.net>
2018-06-23 17:54:46 +0300

    Write database.

--- app/src/main/java/com/example/nimolee/pictureloadertesttask/data/PicturesDataBase.kt
+++ app/src/main/java/com/example/nimolee/pictureloadertesttask/data/PicturesDataBase.kt
@@ -1,4 +1,32 @@
 package com.example.nimolee.pictureloadertesttask.data
 
-class PicturesDataBase {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.example.nimolee.pictureloadertesttask.data.dao.PictureDao
+import com.example.nimolee.pictureloadertesttask.data.entity.PictureEntity
+
+@Database(entities = [PictureEntity::class], version = 1)
+abstract class PicturesDataBase : RoomDatabase() {
+    abstract fun pictureDao(): PictureDao
+
+    companion object {
+        private var INSTANCE: PicturesDataBase? = null
+
+        fun getInstance(context: Context): PicturesDataBase? {
+            if (INSTANCE == null) {
+                synchronized(PicturesDataBase::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                            PicturesDataBase::class.java,
+                            "video.db").build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
 }
